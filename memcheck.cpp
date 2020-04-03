5b6c010998316a2a20fc8d7a61cc9e0a6d678270
Lexa <54638779+synthMoza@users.noreply.github.com>
2020-04-03 15:33:38 +0300

    Alpha 0.2
    Optimized working with the file

--- memcheck.cpp
+++ memcheck.cpp
@@ -11,67 +11,54 @@
 //! delete[](<variable>) => DELETE_(<variable>)
 #define DELETE_(memory) operator delete[](memory, __FILE__, __PRETTY_FUNCTION__, __LINE__)
 
+//!Log file pointer
+FILE* log_file = nullptr;
+
 void* operator new(size_t size, const std::string& file_name, const std::string& func_name, int com_line)
 {
-    FILE* log_file = nullptr;
     void* memory = nullptr;
-
-    log_file = fopen(LOG_FILE_NAME, "a");
 
     memory = ::operator new(size);
 
     fprintf(log_file, "[%p] Memory allocating with \"new\": file "
                                   "\"%s\", function \"%s\", line %d, size %zu.\n", memory, file_name.c_str(), func_name.c_str(), com_line, size);
-    fclose(log_file);
-
     return memory;
 }
 
 void * operator new[](size_t size, const std::string& file_name, const std::string& func_name, int com_line)
 {
-    FILE* log_file = nullptr;
     void* memory = nullptr;
-
-    log_file = fopen(LOG_FILE_NAME, "a");
 
     memory = ::operator new[](size);
 
     fprintf(log_file, "[%p] Memory allocating with \"new[]\": file "
                       "\"%s\", function \"%s\", line %d, size %zu.\n", memory, file_name.c_str(), func_name.c_str(), com_line, size);
-    fclose(log_file);
-
     return memory;
 }
 
 void operator delete(void* memory, const std::string& file_name, const std::string& func_name, int com_line)
 {
-    FILE* log_file = nullptr;
-
-    log_file = fopen(LOG_FILE_NAME, "a");
-
     fprintf(log_file, "[%p] Memory free-up with \"delete\": file "
                       "\"%s\", function \"%s\", line %d.\n", memory, file_name.c_str(), func_name.c_str(), com_line);
-
-    fclose(log_file);
     ::operator delete(memory);
 }
 
 void operator delete[](void* memory, const std::string& file_name, const std::string& func_name, int com_line)
 {
-    FILE* log_file = nullptr;
-
-    log_file = fopen(LOG_FILE_NAME, "a");
-
     fprintf(log_file, "[%p] Memory free-up with \"delete\": file "
                       "\"%s\", function \"%s\", line %d.\n", memory, file_name.c_str(), func_name.c_str(), com_line);
-    fclose(log_file);
     ::operator delete[](memory);
 }
 
-//! Just clears the log file (file name - macro LOG_FILE_NAME)
-void clear_log()
+//! Initializes the log file (file name - macro LOG_FILE_NAME)
+//! This functions must be called at the beginning of "int main()"
+void init_log()
 {
-    FILE* log_file = fopen(LOG_FILE_NAME, "w");
+    log_file = fopen(LOG_FILE_NAME, "w");
+}
+//! Closes the log file
+//! This function must be called at the end of "int main()"
+void close_log()
+{
     fclose(log_file);
 }
-
